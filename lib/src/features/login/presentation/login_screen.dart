827726818ca6953d5c6fc7fba30f2ca8f19f8802
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-06 11:49:48 -0300

    feat: added basic layout for home screen

---
+++ lib/src/features/login/presentation/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  static const route = "login";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
