932012d7ef0f1d7fbed2c4ce0e51f3b3b7dfdb8e
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-06 20:53:26 -0300

    feat: router configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
 import 'package:selecao_app/src/constants/app_theme.dart';
+import 'package:selecao_app/src/routing/router.dart';
+import 'package:selecao_app/src/services/app_service.dart';
+
+final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,16 +29,37 @@ void main() async {
       ),
     );
   }
+
+  // Run default layout
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    //Uses the browser URL's pathname to represent Flutter's route name.
+    usePathUrlStrategy();
+    AppService.instance.initialize();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Seleção App',
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      scaffoldMessengerKey: scaffoldMessengerKey,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: AppTheme.fontFamilyName,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
