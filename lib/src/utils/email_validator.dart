dc9f4df422ef4e66b18a83ff0aa229638c8e25f0
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-06 20:54:58 -0300

    feat: added email validator extension for Strings type

---
+++ lib/src/utils/email_validator.dart
@@ -0,0 +1,11 @@
+extension EmailValidator on String {
+  bool isValidEmail() {
+    return RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+        .hasMatch(this);
+  }
+
+  bool isValidLogin() {
+    return RegExp(r"^[a-zA-Z_]*$").hasMatch(this);
+  }
+}
