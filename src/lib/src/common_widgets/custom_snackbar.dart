122c4a3380dfc8ac023492d839cf1d282e14ab96
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-08 16:57:50 -0300

    feat: added custom snackbar widget

---
+++ src/lib/src/common_widgets/custom_snackbar.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class CustomSnackbar {
+  /// Snackbar to show error messages
+  static SnackBar buildErrorSnackBar({
+    required final String errorString,
+    final int errorDuration = 10,
+  }) {
+    return SnackBar(
+      duration: Duration(seconds: errorDuration),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      content: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            height: 95,
+            decoration: const BoxDecoration(
+              color: Color(0xFFC72C41),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: 30),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Oops!",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      Text(
+                        errorString,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Positioned(
+            top: 20,
+            left: 7,
+            child: CircleAvatar(
+              backgroundColor: Color(0xFF801336),
+              radius: 5,
+            ),
+          ),
+          const Positioned(
+            bottom: 30,
+            left: 15,
+            child: CircleAvatar(
+              backgroundColor: Color(0xFF801336),
+              radius: 7,
+            ),
+          ),
+          const Positioned(
+            bottom: 0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
+              child: CircleAvatar(
+                backgroundColor: Color(0xFF801336),
+                radius: 10,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
