f8b4e57140d97b043a34b13bd9a6a46566a7ccf9
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-08 02:13:46 -0300

    feat: added user model with freezed

---
+++ lib/src/domain/user_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      uuid: json['uuid'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'uuid': instance.uuid,
+      'name': instance.name,
+      'email': instance.email,
+      'password': instance.password,
+    };
