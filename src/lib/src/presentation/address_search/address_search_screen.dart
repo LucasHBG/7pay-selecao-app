ddd9eb84ed54a4d2e733cd13d43e39c40178e359
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-10 03:12:40 -0300

    feat: refactoring address search screen

--- src/lib/src/presentation/address_search/address_search_screen.dart
+++ src/lib/src/presentation/address_search/address_search_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:go_router/go_router.dart';
+import 'package:collection/collection.dart';
+
+import 'package:selecao_app/src/application/services/address_service.dart';
 import 'package:selecao_app/src/application/services/app_service.dart';
+import 'package:selecao_app/src/common_widgets/custom_snackbar.dart';
 import 'package:selecao_app/src/common_widgets/drawer_layout.dart';
+import 'package:selecao_app/src/common_widgets/form_input_decorator.dart';
 import 'package:selecao_app/src/constants/app_theme.dart';
-import 'package:selecao_app/src/presentation/login_screen.dart';
+import 'package:selecao_app/src/domain/viacep_data_store.dart';
+import 'package:selecao_app/src/presentation/address_search/address_card_widget.dart';
+import 'package:selecao_app/src/presentation/login/login_screen.dart';
 import 'package:selecao_app/src/utils/zipcode_pt_br_formatter.dart';
 
 class AddressSearchScreen extends StatefulWidget {
@@ -20,7 +28,11 @@ class _AddressSearchScreenState extends State<AddressSearchScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final TextEditingController _zipcodeController = TextEditingController();
+  final TextEditingController _streetAddressController =
+      TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
 
+  final ViacepDataStore viacepDataStore = ViacepDataStore();
   bool _isLoading = false;
 
   @override
@@ -33,6 +45,7 @@ class _AddressSearchScreenState extends State<AddressSearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text(
           'Endereços',
           style: AppTheme.title,
@@ -51,25 +64,134 @@ class _AddressSearchScreenState extends State<AddressSearchScreen> {
       drawer: const DrawerLayout(),
       body: SingleChildScrollView(
         physics: const ClampingScrollPhysics(),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  _zipcodeInput(),
-                  ElevatedButton(onPressed: () {}, child: const Text("Buscar")),
-                ],
-              ),
-              Row(
-                children: [
-                  // _addressInput(),
-                  // _addressInput(),
-                  ElevatedButton(
-                      onPressed: () {}, child: const Text("Filtrar")),
-                ],
-              ),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              runAlignment: WrapAlignment.start,
+              direction: Axis.horizontal,
+              runSpacing: 25,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                        flex: 6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "CEP:",
+                              style: AppTheme.listItemText,
+                            ),
+                            _zipcodeInput(),
+                          ],
+                        )),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: IconButton(
+                          onPressed: () async {
+                            try {
+                              if (viacepDataStore.viacepDataList.isNotEmpty) {
+                                final searchResult = viacepDataStore
+                                    .viacepDataList
+                                    .firstWhereOrNull(
+                                  (item) => item.cep == _zipcodeController.text,
+                                );
+
+                                if (searchResult != null) {
+                                  throw "CEP já inserido.";
+                                }
+                              }
+
+                              final viacepDataObject = await AddressService()
+                                  .searchZipcode(_zipcodeController.text);
+
+                              viacepDataStore.addItemToList(viacepDataObject);
+                            } catch (e) {
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context).clearSnackBars();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    CustomSnackbar.buildErrorSnackBar(
+                                  errorString: e.toString(),
+                                ));
+                              }
+                            }
+                          },
+                          icon: const Icon(Icons.search, color: Colors.white),
+                          style: IconButton.styleFrom(
+                            shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16))),
+                            padding: const EdgeInsets.all(16),
+                            backgroundColor: AppTheme.itemPrimaryBackground,
+                            side: const BorderSide(
+                              width: 1,
+                              color: Colors.transparent,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Bairro:",
+                            style: AppTheme.listItemText,
+                          ),
+                          const SizedBox(height: 5),
+                          _streetAddressInput(),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Cidade:",
+                            style: AppTheme.listItemText,
+                          ),
+                          const SizedBox(height: 5),
+                          _cityInput(),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 2,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Filtrar",
+                          style: AppTheme.buttonLabel,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Observer(builder: (_) {
+                  return AddressCardWidget(
+                    addressList: viacepDataStore.viacepDataList.toList(),
+                  );
+                })
+              ],
+            ),
           ),
         ),
       ),
@@ -81,7 +203,6 @@ class _AddressSearchScreenState extends State<AddressSearchScreen> {
     return TextFormField(
       controller: _zipcodeController,
       enabled: _isLoading ? false : true,
-      maxLength: 8,
       keyboardType: TextInputType.number,
       autofillHints: const [AutofillHints.postalCode],
       onSaved: (value) =>
@@ -101,28 +222,53 @@ class _AddressSearchScreenState extends State<AddressSearchScreen> {
 
         return null;
       },
+      decoration: inputForm(hintText: "00000-000"),
     );
   } //end widget
 
-  Widget _addressInput() {
+  Widget _streetAddressInput() {
     /// Pode ser uma entrada do tipo E-mail ou Login de acordo com a API
     return TextFormField(
-      controller: _zipcodeController,
+      controller: _streetAddressController,
       enabled: _isLoading ? false : true,
       keyboardType: TextInputType.text,
       onSaved: (value) =>
-          value != null ? _zipcodeController.text = value.trim() : null,
+          value != null ? _streetAddressController.text = value.trim() : null,
       validator: (value) {
         if (value == null || value.isEmpty) {
           return 'O campo não pode estar vazio';
         }
 
         if (value.length >= 8) {
-          return 'CEP inválido';
+          return 'Bairro inválido';
         }
 
         return null;
       },
+      decoration: inputForm(hintText: "Vila Nova"),
+    );
+  } //end widget
+
+  Widget _cityInput() {
+    /// Pode ser uma entrada do tipo E-mail ou Login de acordo com a API
+    return TextFormField(
+      controller: _cityController,
+      enabled: _isLoading ? false : true,
+      keyboardType: TextInputType.text,
+      onSaved: (value) =>
+          value != null ? _cityController.text = value.trim() : null,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'O campo não pode estar vazio';
+        }
+
+        if (value.length >= 8) {
+          return 'UF inválido';
+        }
+
+        return null;
+      },
+      decoration: inputForm(hintText: "GO"),
     );
   } //end widget
 }
