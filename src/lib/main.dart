fe042767b2a7fde35e69e50d093e654065057381
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-06-10 03:06:13 -0300

    feat: refactoring on main

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'package:selecao_app/src/constants/app_theme.dart';
 import 'package:selecao_app/src/routing/router.dart';
@@ -12,40 +11,36 @@ final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (Platform.isIOS || Platform.isAndroid) {
-    await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]).then(
-      (_) => runApp(
-        NotificationListener<OverscrollIndicatorNotification>(
-          onNotification: (overscroll) {
-            overscroll.disallowIndicator();
-            return true;
-          },
-          child: const MyAppMobile(),
-        ),
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    (_) => runApp(
+      NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (overscroll) {
+          overscroll.disallowIndicator();
+          return true;
+        },
+        child: const MyApp(),
       ),
-    );
-  }
-
-  // Run default layout
-  runApp(const MyAppWeb());
+    ),
+  );
 }
 
-class MyAppWeb extends StatefulWidget {
-  const MyAppWeb({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyAppWeb> createState() => _MyAppWebState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppWebState extends State<MyAppWeb> {
+class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
     //Uses the browser URL's pathname to represent Flutter's route name.
     // usePathUrlStrategy();
+    usePathUrlStrategy();
     AppService.instance.initialize();
   }
 
@@ -60,46 +55,38 @@ class _MyAppWebState extends State<MyAppWeb> {
       scaffoldMessengerKey: scaffoldMessengerKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        brightness: Brightness.light,
+        highlightColor: Colors.white,
         fontFamily: AppTheme.fontFamilyName,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: AppTheme.appLightBackground,
+        appBarTheme: const AppBarTheme(color: AppTheme.appLightBackground),
         cardTheme: const CardTheme(
           surfaceTintColor: Colors.white,
+          color: Colors.white,
         ),
-        useMaterial3: true,
-      ),
-    );
-  }
-}
-
-class MyAppMobile extends StatefulWidget {
-  const MyAppMobile({super.key});
-
-  @override
-  State<MyAppMobile> createState() => _MyAppMobileState();
-}
-
-class _MyAppMobileState extends State<MyAppMobile> {
-  @override
-  void initState() {
-    super.initState();
-    AppService.instance.initialize();
-  }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Seleção App',
-      routerDelegate: router.routerDelegate,
-      routeInformationParser: router.routeInformationParser,
-      routeInformationProvider: router.routeInformationProvider,
-      scaffoldMessengerKey: scaffoldMessengerKey,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: AppTheme.fontFamilyName,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        cardTheme: const CardTheme(
-          surfaceTintColor: Colors.white,
+        buttonTheme:
+            const ButtonThemeData(buttonColor: AppTheme.itemPrimaryBackground),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(
+              AppTheme.itemPrimaryBackground,
+            ),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: WidgetStateProperty.all(
+              const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            ),
+            shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16)))),
+            foregroundColor: WidgetStateProperty.all(
+              Colors.white,
+            ),
+            backgroundColor: WidgetStateProperty.all(
+              AppTheme.itemPrimaryBackground,
+            ),
+          ),
         ),
         useMaterial3: true,
       ),
